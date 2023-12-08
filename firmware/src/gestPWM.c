b3fd4450042951d5ea91c99d1b97eb2fef9e222e
Julien Decrausaz <107997286+JulienDecrausaz@users.noreply.github.com>
2023-12-08 14:50:29 +0100

    Update gestPWM.c
    commit du 8.12.23

--- firmware/src/gestPWM.c
+++ firmware/src/gestPWM.c
@@ -38,17 +38,84 @@ void GPWM_Initialize(S_pwmSettings *pData)
 // Obtention vitesse et angle (mise a jour des 4 champs de la structure)
 void GPWM_GetSettings(S_pwmSettings *pData)	
 {
-    // Lecture du convertisseur AD
+    // Lecture des 2 canaux du convertisseur AD
+    S_ADCResults ADCRes;
     
-    // conversion
+    //Tableaux de 10 cases pour stockage des valeurs des 2 canaux de l'ADC
+    uint16_t TabValADC1[10] = {0}; //AD1
+    uint16_t TabValADC2[10] = {0}; //AD2
+
+    //Compteurs pour boucles
+    static uint8_t i = 0; //variable statique pour conserver la valeur après interruption
+    uint8_t j = 0;
     
+    //Variables pour moyennes (initialisées à 0))
+    uint16_t Moyenne1 = 0;
+    uint16_t Moyenne2 = 0;
+    
+    // Variable pour stockage de la valeur brute de l'AD
+    uint16_t ValBruteADC = 0;
+    
+    // Lecture valeur canal 0 de l'AD
+    ADCRes = BSP_ReadAllADC();
+    
+    //===========================CALCUL DES MOYENNES==========================//
+    //MOYENNES GLISSANTES
+    
+    TabValADC1[i] = ADCRes.Chan0; //Lecture canal 1 et mise de la valeur dans le tableau 1
+    TabValADC2[i] = ADCRes.Chan1; //Lecture canal 2 et mise de la valeur dans le tableau 2
+    
+    //Changement de case du tableau (vers la droite)
+    if(i < 10)
+    {
+        i++;
+    }
+    else //Si on est tout à droite, on reviens au début
+    {
+        i = 0;
+    }
+     
+    //Récupération des valeurs du tableau
+    for(j = 0; j < 10; j++)
+    {
+        Moyenne1 += TabValADC1[j];
+        Moyenne2 += TabValADC2[j];
+    }
+    
+    Moyenne1 = Moyenne1 / 10;
+    Moyenne2 = Moyenne2 / 10;
+    
+    //========================================================================//
+    //Conversion Vitesse
+    ValBruteADC = (Moyenne1*198) / 1023;
+    
+    PWMData.SpeedSetting = ValBruteADC - 99;
+    if(PWMData.SpeedSetting < 0)
+    {
+        PWMData.absSpeed = PWMData.SpeedSetting * -1;
+    }
+    else
+    {
+        PWMData.absSpeed = PWMData.SpeedSetting;
+    }
+    
+    //Conversion Angle
+    PWMData.AngleSetting = ((Moyenne2*180) / 1023) - 90;
 }
 
 
 // Affichage des information en exploitant la structure
 void GPWM_DispSettings(S_pwmSettings *pData)
 {
-    
+    // Affichage de la vitesse du moteur (valeur signée)
+    lcd_gotoxy(1,2);
+    printf_lcd("SpeedSetting %3d", pData -> SpeedSetting);
+    // Affichage de la vitesse du moteur (valeur absolue)
+    lcd_gotoxy(1,3);
+    printf_lcd("absSpeed %7d", pData -> absSpeed);
+    // Affichage de l'angle du servomoteur (valeur signée)
+    lcd_gotoxy(1,4);
+    printf_lcd("Angle %10d", pData -> AngleSetting);
 }
 
 // Execution PWM et gestion moteur à partir des info dans structure
